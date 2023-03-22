6f97457b20cb5829e4af6c15206e3e5c3419ec60
“Mohamed <MohamedGamal.Dv@gmail.com>
2023-03-22 06:49:27 +0200

    Add login Auth check & successful login setup

---
+++ static/js/dash-content.js
@@ -0,0 +1,12 @@
+const dashContent = `
+<nav>
+<ul class="dash-nav">
+  <li class="dash-nav-btn" id="add-quote">
+    <a href="#">Request Quote</a>
+  </li>
+  <li class="dash-nav-btn"  id="view-quotes">
+    <a href="#">View all Quotes</a>
+  </li>
+</ul>
+</nav>
+`;
