4b30805b698be5a5a709dd29b35ab3ce7a8ded28
“Mohamed <MohamedGamal.Dv@gmail.com>
2023-03-22 06:46:46 +0200

    Add "layout" main navbar styles setup

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello console!')
+// Build Hamburger menu toggler
+// Initial setup
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+// toggler onClick functionality
+function toggleMenu() {
+  if (menu.classList.contains('active')) {
+    // Handle if its open:
+    // Close and Show Hamburger icon
+    menu.classList.remove('active');
+    toggle.querySelector('a').innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    // Handle if its close:
+    // Open and Show close icon
+    menu.classList.add('active');
+    toggle.querySelector('a').innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+// Trigger the toggler when menu clicked
+toggle.addEventListener('click', toggleMenu);
