b9b185ee8e4091a75c616e1d9b1a6ff5aace3011
“Mohamed <MohamedGamal.Dv@gmail.com>
2023-03-22 06:35:55 +0200

    Add create record "POST" functionality

---
+++ server/app.py
@@ -0,0 +1,44 @@
+# Initial Setup
+import json
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+db_path = 'db/db.txt'
+
+# API setup:
+# ===================================== #
+# # Create (POST) Functionality:
+# ## Record-Builder helper function
+# ===================================== #
+
+
+def build_record(id, name, idea, url):
+    record = {
+        "id": id,
+        "name": name,
+        "idea": idea,
+        "url": url,
+    }
+    # Add new record to db file and create the file if not exist
+    with open(db_path, "a+") as db_file:
+        db_file.write(f"{json.dumps(record)} \n")
+
+
+# ## Create Record (Handle POST request)
+@app.route('/', methods=['POST'])
+def create_record():
+    # Check guard : JSON data only (or)
+    # If Incoming request not including "id" which means its invalid or empty
+    # Cancel request and send BAD request error
+    if not request.json or not "id" in request.json:
+        abort(400)
+
+    # Build record
+    r_id = request.json["id"]
+    r_name = request.json["name"]
+    r_idea = request.json["idea"]
+    r_url = request.json["url"]
+
+    build_record(r_id, r_name, r_idea, r_url)
+
+    return jsonify({"message": "Created!", "success": True})
